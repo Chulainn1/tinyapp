195bfce63bcd0bfd4b013486991a062d9c86733d
leam <leam.murphy1@gmail.com>
2021-06-07 22:03:22 +0000

    added a ejs file to render the urls in the views directory

--- express_server.js
+++ express_server.js
@@ -9,16 +9,24 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase};
+
+  res.render("urls_index", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Greetings!");
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get("/hello", (req, res) =>{
   res.send("<html><body>Hello <b>World</b></body></html>\n")
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
-})
+});
