08dcf7d71bc873192eb6b28d3a869a93fa81a526
leam <leam.murphy1@gmail.com>
2021-06-07 20:39:38 +0000

    Added a GET request that provides the entire urlDatabase object as a JSON string.

--- express_server.js
+++ express_server.js
@@ -12,6 +12,9 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Greetings!");
 });
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
