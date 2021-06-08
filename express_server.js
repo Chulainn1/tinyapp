ff588bd719efd0256ae6c4c04239a034696e3758
leam <leam.murphy1@gmail.com>
2021-06-08 00:40:59 +0000

    added a post req that responds with a generated random string.

--- express_server.js
+++ express_server.js
@@ -7,6 +7,17 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+function generateRandomString() {
+  let alnu = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let len = 6;
+  let result = '';
+  for (let i = len; i > 0; --i) {
+    result += alnu[Math.floor(Math.random() * alnu.length)];
+  }
+  return result;
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -27,20 +38,10 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars)
 });
 
-
-
-
-// app.get("/", (req, res) => {
-//   res.send("Greetings!");
-// });
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) =>{
-//   res.send("<html><body>Hello <b>World</b></body></html>\n")
-// });
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
