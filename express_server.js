036658ad6c9b0786ac181d05ce17d27feab70888
leam <leam.murphy1@gmail.com>
2021-06-09 04:33:03 +0000

    added a login feature that will store your username with a cookie and display that you are logged in. Currently implementing the logout logic

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express"); 
 const app = express();
+const cookieParser = require('cookie-parser')
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(cookieParser())
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -24,17 +26,26 @@ const urlDatabase = {
 };
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase};
-
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 })
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars)
 });
 
@@ -44,7 +55,7 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  // console.log(req.body);  
   let randoStr = generateRandomString();
   urlDatabase[randoStr] = req.body.longURL;
   res.redirect(`/urls/${randoStr}`);
@@ -53,16 +64,16 @@ app.post("/urls", (req, res) => {
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURLToBeDeleted = req.params.shortURL;
-  console.log("deleted shortURL:", shortURLToBeDeleted)
+  // console.log("deleted shortURL:", shortURLToBeDeleted)
   delete urlDatabase[shortURLToBeDeleted];
 
-  res.redirect('/urls/:shortURL');
+  res.redirect('/urls');
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {
   const shortURLToBeEdited = req.params.shortURL;
-  console.log("editing shortURL:", shortURLToBeEdited)
-  console.log(req.body.longURL)
+  // console.log("editing shortURL:", shortURLToBeEdited)
+  // console.log(req.body.longURL)
   if (urlDatabase[shortURLToBeEdited] && req.body.longURL) {
     urlDatabase[shortURLToBeEdited] = req.body.longURL;
   } else {
@@ -71,6 +82,23 @@ app.post("/urls/:shortURL/edit", (req, res) => {
 
   res.redirect('/urls');
 });
+
+app.post("/login", (req, res) => {
+  console.log(typeof req.body.username)
+  res.cookie("username", req.body.username)
+
+  res.redirect('/urls');
+});
+
+app.post("/logout", (req, res) => {
+  console.log(typeof req.body.username)
+  res.cookie("username", req.body.username)
+
+  res.redirect('/urls');
+});
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
