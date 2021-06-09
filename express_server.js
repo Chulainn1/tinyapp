b8eb0e795c5e58fbc08fadf51a0ecf3f0ba2b3a7
leam <leam.murphy1@gmail.com>
2021-06-09 22:57:58 +0000

    added a registration page that acceps an email and password. It creates an id and a cookie for that id. It stores your id, email,password in a users database.

--- express_server.js
+++ express_server.js
@@ -9,7 +9,6 @@ app.use(cookieParser());
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-// ----------------------------Day 1 ------------------------------ //
 function generateRandomString() {
   let alnu = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let len = 6;
@@ -18,33 +17,59 @@ function generateRandomString() {
     result += alnu[Math.floor(Math.random() * alnu.length)];
   }
   return result;
-}
+};
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "qwe": {
+    id: "qwe", 
+    email: "a@a.com", 
+    password: "123"
+  },
+ "asd": {
+    id: "asd", 
+    email: "b@b.com", 
+    password: "321"
+  }
+
+}
+
+
+
+
 app.get("/urls", (req, res) => {
+  console.log(req.cookies)
+  const id = req.cookies["user_id"];
+  const user = users[id];
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user 
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
+  const id = req.cookies["user_id"];
+  const user = users[id];
   const templateVars = {
-    username: req.cookies["username"]
+    user
   };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
+  const id = req.cookies["user_id"];
+  const user = users[id];
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"]
+    user
   };
   res.render("urls_show", templateVars);
 });
@@ -53,6 +78,15 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
+app.get("/register", (req, res) => {
+  const templateVars = { user: null }
+  res.render("register", templateVars);
+})
+
+
+
+
 
 app.post("/urls", (req, res) => {
   // console.log(req.body);
@@ -64,7 +98,6 @@ app.post("/urls", (req, res) => {
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURLToBeDeleted = req.params.shortURL;
-  // console.log("deleted shortURL:", shortURLToBeDeleted)
   delete urlDatabase[shortURLToBeDeleted];
 
   res.redirect('/urls');
@@ -72,27 +105,53 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 app.post("/urls/:shortURL/edit", (req, res) => {
   const shortURLToBeEdited = req.params.shortURL;
-  // console.log("editing shortURL:", shortURLToBeEdited)
-  // console.log(req.body.longURL)
+
   if (urlDatabase[shortURLToBeEdited] && req.body.longURL) {
     urlDatabase[shortURLToBeEdited] = req.body.longURL;
   } else {
     return res.json({error: "short url does not exist"});
   }
-
   res.redirect('/urls');
 });
 
 app.post("/login", (req, res) => {
   console.log(typeof req.body.username);
-  res.cookie("username", req.body.username);
+  res.cookie("user", req.body.email);
 
   res.redirect('/urls');
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user");
   res.redirect('/urls');
+});
+
+const emailLookup = function (email, usersData) {
+  for(const user in usersData) {
+    if (usersData[user].email === email) {
+      return usersData[user];
+    }
+  }
+  return 
+}; 
+
+app.post("/register", (req, res) => {
+  const password = req.body.password;
+  const email = req.body.email;
+
+  if (!password || !email) {
+    return res.status(400).send("An email AND password are requried");
+  }
+  if(emailLookup(email, users)) {
+    return res.status(400).send(`An account already exists under ${usersData[user]}`)
+  }
+
+  const id = generateRandomString();
+  const user = { id, email, password }
+  users[id] = user;
+  console.log(user)
+  res.cookie("user_id", id)
+  res.redirect("/urls");
 });
 
 
@@ -101,3 +160,5 @@ app.post("/logout", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
